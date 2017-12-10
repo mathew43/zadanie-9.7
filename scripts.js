8678d679b0e5263511f15a69daae7789401f96f1
Mateusz Łukowski <mateusz.lukowski@wp.pl>
2017-12-10 20:03:16 +0100

    add listeners on pick options

--- scripts.js
+++ scripts.js
@@ -1,2 +1,10 @@
 var newGameBtn = document.getElementById('js-newGameButton');
 newGameBtn.addEventListener('click', newGame);
+
+var pickRock = document.getElementById('js-playerPick_rock'),
+	pickPaper = document.getElementById('js-playerPick_paper'),
+	pickScissors = document.getElementById('js-playerPick_scissors');
+
+pick.Rock.addEventListener('click', function() {playerPick('rock')});
+pick.Paper.addEventListener('click', function() {playerPick('paper')});
+pick.Scissors.addEventListener('click', function() {playerPick('scissors')});
